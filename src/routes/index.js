1aeacbc101860c02f08c3617d3667f49e9890398
Joe Smith <joesmith0488@gmail.com>
2016-01-18 12:48:22 +0000

    created algorithim to generate and store shortened urls with tests

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,12 @@ const path = require('path');
 
 const app = express();
 
+const sug = require('../utils/shortUrlGenerator');
+
 app.use('/', express.static(path.join(__dirname, '../public')));
 
+app.get('/new/?*', (req, res) => {
+  res.json(sug(req.params[0]))
+});
 
 module.exports = app;
--- test/routes-test.js
+++ test/routes-test.js
@@ -18,4 +18,52 @@ describe('Express Routes', () => {
         .expect('Content-Type', /html/, done);
     });
   });
+
+  describe('To URL shorterner API', () => {
+    it('Retruns 200 status', (done) => {
+      request(app)
+        .get('/new/http://www.example.com')
+        .expect(200, done)
+    });
+    it('Returns JSON format', (done) => {
+      request(app)
+        .get('/new/http://www.example.com')
+        .expect('Content-Type', /json/, done);
+    });
+    it('Returns a URL not found error if no params are provided', (done) => {
+      const expectedResponse = {
+        error: 'No URL found'
+      };
+      request(app)
+        .get('/new/')
+        .expect((res) => {
+          expect(res.body).to.deep.equal(expectedResponse);
+        })
+        .end(done);
+    })
+    it('Returns an invalid URL error if url is not valid format', (done) => {
+      const expectedResponse = {
+        error: 'URL invalid'
+      };
+      request(app)
+        .get('/new/blibidyboop')
+        .expect((res) => {
+          expect(res.body).to.deep.equal(expectedResponse);
+        })
+        .end(done);
+    });
+    it(`Returns an object with the original url and
+        the short url if the URL provided is valid`, (done) => {
+      const expectedResponse = {
+        original_url: 'htttp://www.example.com',
+        short_url: 'http:/localhost:3000/2'
+      };
+      request(app)
+        .get('/new/htttp://www.example.com')
+        .expect((res) => {
+          expect(res.body).to.deep.equal(expectedResponse);
+        })
+        .end(done);
+    });
+  });
 });
